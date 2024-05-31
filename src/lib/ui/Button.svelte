9210050358f8931f5690f9370bed215e47f02ee4
Samsonium <tdesu@vk.com>
2024-06-01 02:23:42 +0300

    Add Button component

---
+++ src/lib/ui/Button.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+    /** Should button have accent style */
+    export let accent: boolean = false;
+</script>
+
+<button class:accent on:click>
+    {#if $$slots.icon}
+        <div class="icon">
+            <slot name="icon"/>
+        </div>
+    {/if}
+    <span><slot/></span>
+</button>
+
+<style>
+    button {
+        padding: 8px 12px 8px 0;
+        border-radius: 10px;
+        display: flex;
+        flex-flow: row nowrap;
+        background: transparent;
+        border: none;
+        outline: 2px solid transparent;
+        transition: padding-left .2s ease-out,
+                    outline-color .2s ease-out;
+        
+        --icon: var(--text);
+        
+        &:hover {
+            outline-color: var(--subtext);
+            padding-left: 10px;
+        }
+        
+        & .icon {
+            margin-right: 9px;
+        }
+        
+        & span {
+            font: 500 18px Inter, sans-serif;
+            color: var(--text);
+        }
+        
+        &.accent {
+            background: var(--accent);
+            padding-left: 10px;
+        }
+    }
+</style>
