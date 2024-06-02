52fe4e5d39f466c7eef0080ab4886bb6d8cfccc3
Samsonium <tdesu@vk.com>
2024-06-02 18:11:33 +0300

    Add Title component

---
+++ src/lib/ui/Title.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	/** Title content */
+	export let text: string = '';
+</script>
+
+<h2>{text}</h2>
+
+<style>
+	h2 {
+		font:
+			900 18px Inter,
+			sans-serif;
+		color: white;
+		padding: 0;
+		margin: 40px 0 20px;
+	}
+</style>
