fe9b583c310ff8689faf5f76eb0ef7706ef00b34
Samsonium <tdesu@vk.com>
2024-06-02 18:05:44 +0300

    Add title and padding to Page component

--- src/lib/ui/Page.svelte
+++ src/lib/ui/Page.svelte
@@ -1,19 +1,35 @@
 <script lang="ts">
-    /** Page title */
-    export let title: string;
+	/** Page title */
+	export let title: string;
 </script>
 
 <svelte:head>
-    <title>{title}</title>
+	<title>{title}</title>
 </svelte:head>
 
 <div class="page">
-    <slot/>
+	<h4>{title}</h4>
+	<slot />
 </div>
 
 <style>
-    .page {
-        width: 100%;
-        height: 100%;
-    }
+	.page {
+		width: 100%;
+		height: 100%;
+		overflow-x: hidden;
+		overflow-y: auto;
+		padding: 0 24px;
+	}
+
+	h4 {
+		font:
+			400 14px Inter,
+			sans-serif;
+		color: #9f9f9f;
+		letter-spacing: 4px;
+		width: 100%;
+		padding: 12px 0;
+		text-align: center;
+		vertical-align: middle;
+	}
 </style>
